a4c89beb3193b4f5991e94682b7b9019aa6cc590
GustavoAquinoPaz <42361589+GustavoAquinoPaz@users.noreply.github.com>
2018-08-13 20:37:51 -0500

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,21 @@
+/** mai.c Este es un archivo Hola Mundo
+* incluido en este curso -- solo para evitar
+* la maldici\'on del programador -- En este curso,
+* usted aprender\'a a usar correctamente muchos de 
+* los elementos de lenguaje C. Especialmente, usted
+* deber\'a aprender usar apuntadores, de lo contrario usted
+* sufrir\'a lo indencible y un perro salvaje lo
+
+*/
+#include <stdio.h>/* int printf(chair*, ...)*/
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  putchar('\n');
+  printf("Hola Mundo C!!\n");
+  system("PAUSE");	
+  return 0;
+}
